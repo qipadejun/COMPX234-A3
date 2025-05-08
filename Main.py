03b187aa8c58d3615908dd6031b69e860ff6529f
qipadejun <2758322525@qq.com>
2025-05-09 00:05:42 +0800

    Fix bugs

--- Main.py
+++ Main.py
@@ -5,20 +5,20 @@ import signal
 
 def start_server(port):
     # Start the server process
-    cmd = ["python", "server.py", str(port)]
+    cmd = ["python", "Server.py", str(port)]
     return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 def run_clients_sequentially(port, num_clients):
     # Run the client sequentially
     for i in range(1, num_clients+1):
-        cmd = ["python", "client.py", "localhost", str(port), f"client-{i}.txt"]
+        cmd = ["python", "Client.py", "localhost", str(port), f"client-{i}.txt"]
         subprocess.run(cmd, check=True)
 
 def run_clients_concurrently(port, num_clients):
     # Concurrent running of client
     processes = []
     for i in range(1, num_clients+1):
-        cmd = ["python", "client.py", "localhost", str(port), f"client-{i}.txt"]
+        cmd = ["python", "Client.py", "localhost", str(port), f"client-{i}.txt"]
         p = subprocess.Popen(cmd)
         processes.append(p)
     return processes
